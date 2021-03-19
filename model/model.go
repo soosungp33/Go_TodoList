17e533b54d7215c64e6392f16295c2a8a5a78537
soosungp33 <soosungp33@gmail.com>
2021-03-19 17:08:35 +0900

    모델과 memory부분 분리하기

--- model/model.go
+++ model/model.go
@@ -17,48 +17,10 @@ type dbHandler interface {
 	completeTodo(id int, complete bool) bool
 }
 
-type memoryHandler struct {
-	todoMap map[int]*Todo
-}
-
-func (m *memoryHandler) getTodos() []*Todo {
-	list := []*Todo{}
-	for _, v := range m.todoMap {
-		list = append(list, v)
-	}
-	return list
-}
-func (m *memoryHandler) addTodo(name string) *Todo {
-	id := len(m.todoMap) + 1 // 임의의 ID
-	todo := &Todo{id, name, false, time.Now()}
-	m.todoMap[id] = todo
-	return todo
-}
-func (m *memoryHandler) removeTodo(id int) bool {
-	if _, ok := m.todoMap[id]; ok { // todoMap에 id가 있으면
-		delete(m.todoMap, id) // 삭제하고
-		return true
-	}
-	return false
-}
-func (m *memoryHandler) completeTodo(id int, complete bool) bool {
-	if todo, ok := m.todoMap[id]; ok {
-		todo.Completed = complete // true면 체크, false면 해제
-		return true
-	}
-	return false
-}
-
-func newMemoryHandler() dbHandler {
-	m := &memoryHandler{}
-	m.todoMap = make(map[int]*Todo)
-	return m
-}
-
 var handler dbHandler
 
 func init() { // 이 패키지가 initialize될 때 한 번만 호출됨(초기화)
-	handler = newMemoryHandler()
+	handler = newMemoryHandler() // memoryHandler.go에 있음
 }
 
 func GetTodos() []*Todo {
