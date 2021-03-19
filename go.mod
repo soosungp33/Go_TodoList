fca4515db561b2382eddc9a15b83206caea77916
soosungp33 <soosungp33@gmail.com>
2021-03-19 16:39:20 +0900

    db를 사용하기 위한 코드 리펙토링

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	github.com/stretchr/testify v1.7.0
 	github.com/unrolled/render v1.0.3 // indirect
 	github.com/urfave/negroni v1.0.0 // indirect
--- model/model.go
+++ model/model.go
@@ -10,39 +10,69 @@ type Todo struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-var todoMap map[int]*Todo // 인메모리 db역할
-
-func init() { // 이 패키지가 initialize될 때 한 번만 호출됨(초기화)
-	todoMap = make(map[int]*Todo)
+type dbHandler interface {
+	getTodos() []*Todo
+	addTodo(name string) *Todo
+	removeTodo(id int) bool
+	completeTodo(id int, complete bool) bool
 }
 
-func GetTodos() []*Todo {
+type memoryHandler struct {
+	todoMap map[int]*Todo
+}
+
+func (m *memoryHandler) getTodos() []*Todo {
 	list := []*Todo{}
-	for _, v := range todoMap {
+	for _, v := range m.todoMap {
 		list = append(list, v)
 	}
 	return list
 }
-
-func AddTodo(name string) *Todo {
-	id := len(todoMap) + 1 // 임의의 ID
+func (m *memoryHandler) addTodo(name string) *Todo {
+	id := len(m.todoMap) + 1 // 임의의 ID
 	todo := &Todo{id, name, false, time.Now()}
-	todoMap[id] = todo
+	m.todoMap[id] = todo
 	return todo
 }
-
-func RemoveTodo(id int) bool {
-	if _, ok := todoMap[id]; ok { // todoMap에 id가 있으면
-		delete(todoMap, id) // 삭제하고
+func (m *memoryHandler) removeTodo(id int) bool {
+	if _, ok := m.todoMap[id]; ok { // todoMap에 id가 있으면
+		delete(m.todoMap, id) // 삭제하고
 		return true
 	}
 	return false
 }
-
-func CompleteTodo(id int, complete bool) bool {
-	if todo, ok := todoMap[id]; ok {
+func (m *memoryHandler) completeTodo(id int, complete bool) bool {
+	if todo, ok := m.todoMap[id]; ok {
 		todo.Completed = complete // true면 체크, false면 해제
 		return true
 	}
 	return false
+}
+
+func newMemoryHandler() dbHandler {
+	m := &memoryHandler{}
+	m.todoMap = make(map[int]*Todo)
+	return m
+}
+
+var handler dbHandler
+
+func init() { // 이 패키지가 initialize될 때 한 번만 호출됨(초기화)
+	handler = newMemoryHandler()
+}
+
+func GetTodos() []*Todo {
+	return handler.getTodos()
+}
+
+func AddTodo(name string) *Todo {
+	return handler.addTodo(name)
+}
+
+func RemoveTodo(id int) bool {
+	return handler.removeTodo(id)
+}
+
+func CompleteTodo(id int, complete bool) bool {
+	return handler.completeTodo(id, complete)
 }
